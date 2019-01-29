1412a82ab867696a5d89968e0f0c26caa0c048a2
Daniel Tymecki <daniel.tymecki@gmail.com>
2019-01-29 11:57:15 -0500

    Added Basics of Unit Stats

--- Test/Assets/Scripts/Unit.cs
+++ Test/Assets/Scripts/Unit.cs
@@ -31,6 +31,49 @@ public class Stat {
     }
 }
 
+public enum StatType {
+    Health,
+    Stamina,
+    Strength,
+    Skill,
+    Endurance,
+    Magic,
+    Defense,
+    Resistance,
+    Luck,
+    Speed,
+    Movement
+}
+
+public class StatFactory {
+    public Stat GetStat(StatType type, int startValue=10, int min=1, int max=80){
+        switch (type) {
+            case type.Health:
+                return new Stat("Health","The amount of hits you can take before your unit dies.",startValue,max,min);
+            case type.Stamina:
+                return new Stat("Stamina","The amount of actions you can take before inflicted with exhaustion.",startValue,max,min);
+            case type.Strength:
+                return new Stat("Strength","The amount of damage you inflict through physical weapons.",startValue,max,min);
+            case type.Skill:
+                return new Stat("Skill","Used to determine hit and trigger skills.",startValue,max,min);
+            case type.Endurance:
+                return new Stat("Endurance","Increases the amount of stamina alloted in a battle.",startValue,max,min);
+            case type.Magic:
+                return new Stat("Magic","The amount of damage you inflict through magical attacks.",startValue,max,min);
+            case type.Defense:
+                return new Stat("Defense","Reduces the damage taken from physical attacks.",startValue,max,min);
+            case type.Resistance:
+                return new Stat("Resistance","Reduces the damage taken from magical attacks.",startValue,max,min);
+            case type.Luck:
+                return new Stat("Luck","Determine critical chance / avoid amoung other things.",startValue,max,min);
+            case type.Speed:
+                return new Stat("Speed","Boosts evasion and accuracy and potential increase actions performed.",startValue,max,min);
+            case type.Movement:
+                return new Stat("Movement","Determines the number of tiles moved in a single in a turn.",startValue,max,min);
+        }
+    }
+}
+
 
 public class Unit {
     Stat Health;
@@ -44,6 +87,25 @@ public class Unit {
     Stat Luck;
     Stat Speed;
     Stat Movement;
+    string name;
+    // Class 
+    // Subclass
+
+    public Unit(string Name = "Unit"){
+        name = Name;
+        StatFactory sf = new StatFactory();
+        Health = sf.GetStat(StatType.Health);
+        Stamina = sf.GetStat(StatType.Stamina);
+        Strength = sf.GetStat(StatType.Strength);
+        Skill = sf.GetStat(StatType.Skill);
+        Endurance = sf.GetStat(StatType.Endurance);
+        Magic = sf.GetStat(StatType.Magic);
+        Defense = sf.GetStat(StatType.Defense);
+        Resistance = sf.GetStat(StatType.Resistance);
+        Luck = sf.GetStat(StatType.Luck);
+        Speed = sf.GetStat(StatType.Speed);
+        Movement = sf.GetStat(StatType.Movement,4,8);
+    }
 
 
 }
